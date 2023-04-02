cd9951c451c26b2fef8a5de0e9480017412d31e5
Dmitry Buiko <bdv.s4kt@gmail.com>
2023-04-01 20:32:09 -0500

    Add problem description to howSum

--- howSum.go
+++ howSum.go
@@ -1,5 +1,17 @@
 package dynamic
 
+/*
+Write a function `howSum(targetSum, numbers)` that takes in
+a targetSum and an array of numbers as arguments.
+
+The function should return an array containing any combinations
+of elements that add up to exactly the targetSum. If there is no
+combination that adds up to the targetSum, then return null.
+
+If there are multiple combinations possible, you may
+return any single one.
+*/
+
 func howSum(target int, nums []int) []int {
 	if target == 0 {
 		return []int{}
