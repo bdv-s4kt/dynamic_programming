ba743020582e568fae53561c088c368e7de862c2
Dmitry Buiko <bdv.s4kt@gmail.com>
2023-04-01 20:34:54 -0500

    Add problem description to Fib

--- fib.go
+++ fib.go
@@ -1,5 +1,13 @@
 package dynamic
 
+/*
+Write a function `fib(n)` that takes in a number as an argument.
+The function should return the n-th number of the Fibonacci sequence.
+
+The 1st and 2nd number of the sequence is 1.
+To generate the next number of the sequence, we sum the previous two.
+*/
+
 func fib(num int) int {
 	if num <= 2 {
 		return 1
