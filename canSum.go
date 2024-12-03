81179a516619b35aeb14d8d45d868a065b4bc9f4
Dmitry Buiko <bdv.s4kt@gmail.com>
2024-12-03 16:13:39 -0500

    Fix typo

--- canSum.go
+++ canSum.go
@@ -7,9 +7,9 @@ a targetSum and an array of numbers as arguments.
 The function should return a boolean indicating whether or not it is
 possible to generate the targetSum using numbers from the array.
 
-You may use an elemtn of the array as many times as needed.
+You may use an element of the array as many times as needed.
 
-You may assume that all input numbers are nonnegative.
+You may assume that all input numbers are non-negative.
 */
 
 func canSum(target int, arr []int) bool {
