944bc371b78540001626cdc980246fe69a70418e
Dmitry Buiko <bdv.s4kt@gmail.com>
2024-12-02 21:08:19 -0500

    Add time/space complexities for gridTraveler problem & optimized tabulation version

--- gridtraveler.go
+++ gridtraveler.go
@@ -11,16 +11,22 @@ In how many ways can you travel to the goal on a grid with
 dimensions m * n?
 
 Write a function `gridTraveler(m, n)` that calculate this.
+
+https://leetcode.com/problems/unique-paths/description/
 */
 
-func gridTraveler(m, n int) int {
+// Recursive solution
+// Time: O(2^(m+n)), Space: O(m+n)
+func gridTraveler_recur(m, n int) int {
 	if m == 1 || n == 1 {
 		return 1
 	}
 
-	return gridTraveler(m-1, n) + gridTraveler(m, n-1)
+	return gridTraveler_recur(m-1, n) + gridTraveler_recur(m, n-1)
 }
 
+// Recursive + memoization
+// Time: O(m*n), Space: O(m+n)
 func gridTraveler_memo(mm, nn int) int {
 	memo := map[string]int{}
 
@@ -44,6 +50,37 @@ func gridTraveler_memo(mm, nn int) int {
 	return f(mm, nn)
 }
 
+// Optimized version of memoization approach,
+// storing not only (x:y), but also (y:x) as well
+// Time: O(m*n), Space: O(m+n)
+func gridTraveler_memo_optim(mm, nn int) int {
+	memo := map[string]int{}
+
+	var f func(int, int) int
+
+	f = func(m, n int) int {
+		if m == 1 || n == 1 {
+			return 1
+		}
+		ind := fmt.Sprintf("%d:%d", m, n)
+		res, ok := memo[ind]
+		if ok {
+			return res
+		}
+
+		res = f(m-1, n) + f(m, n-1)
+
+		memo[ind] = res
+		memo[fmt.Sprintf("%d:%d", n, m)] = res
+
+		return memo[ind]
+	}
+
+	return f(mm, nn)
+}
+
+// Tabulation version
+// Time: O(m*n), Space: O(m*n)
 func gridTraveler_tab(m, n int) int {
 	tab := make([][]int, m)
 	for i := range tab {
@@ -66,4 +103,38 @@ func gridTraveler_tab(m, n int) int {
 		}
 	}
 	return tab[m-1][n-1]
+}
+
+// Optimized tabulation, use only 2 rows (or columns, depending on what is smaller)
+// Time: O(m*n), Space: O(min(m, n))
+func gridTraveler_tab_optim(m, n int) int {
+	min := min(m, n)
+	max := max(m, n)
+
+	a := make([]int, min)
+	b := make([]int, min)
+
+	for i := 0; i < max; i++ {
+		b[0] = 1
+		for j := 1; j < min; j++ {
+			b[j] = b[j-1] + a[j]
+		}
+		a, b = b, a
+	}
+
+	return a[min - 1]
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
--- gridtraveler_test.go
+++ gridtraveler_test.go
@@ -6,10 +6,10 @@ import (
 )
 
 func Test_gridTraveler(t *testing.T) {
-	if gridTraveler(3, 3) != 6 {
+	if gridTraveler_recur(3, 3) != 6 {
 		t.Error(`should be 6`)
 	}
-	x := gridTraveler(4, 3)
+	x := gridTraveler_recur(4, 3)
 	if x != 10 {
 		t.Errorf("value [%d], should be [10]", x)
 	}
@@ -25,6 +25,16 @@ func Test_gridTraveler_memo(t *testing.T) {
 	}
 }
 
+func Test_gridTraveler_memo_optim(t *testing.T) {
+	if gridTraveler_memo_optim(3, 3) != 6 {
+		t.Error(`should be 6`)
+	}
+	x := gridTraveler_memo_optim(4, 3)
+	if x != 10 {
+		t.Errorf("value [%d], should be [10]", x)
+	}
+}
+
 func Test_gridTraveler_tab(t *testing.T) {
 	if gridTraveler_tab(3, 3) != 6 {
 		t.Error(`should be 6`)
@@ -35,14 +45,24 @@ func Test_gridTraveler_tab(t *testing.T) {
 	}
 }
 
+func Test_gridTraveler_tab_optim(t * testing.T) {
+	if gridTraveler_tab_optim(3, 3) != 6 {
+		t.Error(`should be 6`)
+	}
+	x := gridTraveler_tab_optim(4, 3)
+	if x != 10 {
+		t.Errorf("value [%d], should be [10]", x)
+	}
+}
+
 // benchmarks
 
-func Benchmark_gridTraveler(b *testing.B) {
+func Benchmark_gridTraveler_recur(b *testing.B) {
 	testCases := [][]int{{2, 3}, {3, 3}, {4, 4}, {18, 18}}
 	for _, testCase := range testCases {
 		b.Run(fmt.Sprintf("array %d x %d", testCase[0], testCase[1]), func(b *testing.B) {
 			for i := 0; i < b.N; i++ {
-				gridTraveler(testCase[0], testCase[1])
+				gridTraveler_recur(testCase[0], testCase[1])
 			}
 		})
 	}
@@ -59,12 +79,35 @@ func Benchmark_gridTraveler_memo(b *testing.B) {
 	}
 }
 
+// 
+func Benchmark_gridTraveler_memo_optim(b *testing.B) {
+	testCases := [][]int{{2, 3}, {3, 3}, {4, 4}, {18, 18}, {50, 50}}
+	for _, testCase := range testCases {
+		b.Run(fmt.Sprintf("array %d x %d", testCase[0], testCase[1]), func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				gridTraveler_memo_optim(testCase[0], testCase[1])
+			}
+		})
+	}
+}
+
 func Benchmark_gridTraveler_tab(b *testing.B) {
 	testCases := [][]int{{2, 3}, {3, 3}, {4, 4}, {18, 18}, {50, 50}}
 	for _, testCase := range testCases {
 		b.Run(fmt.Sprintf("array %d x %d", testCase[0], testCase[1]), func(b *testing.B) {
 			for i := 0; i < b.N; i++ {
 				gridTraveler_tab(testCase[0], testCase[1])
+			}
+		})
+	}
+}
+
+func Benchmark_gridTraveler_tab_optim(b *testing.B) {
+	testCases := [][]int{{2, 3}, {3, 3}, {4, 4}, {18, 18}, {50, 50}}
+	for _, testCase := range testCases {
+		b.Run(fmt.Sprintf("array %d x %d", testCase[0], testCase[1]), func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				gridTraveler_tab_optim(testCase[0], testCase[1])
 			}
 		})
 	}
